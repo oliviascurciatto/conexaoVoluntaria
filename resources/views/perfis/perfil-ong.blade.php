d8de2d37e38884194e26285b86a4c18c17b74012
Olivia Scurciatto Fernandes <olivia.scurciatto@yahoo.com>
2023-08-23 20:09:51 -0300

    .

--- resources/views/perfis/perfil-ong.blade.php
+++ resources/views/perfis/perfil-ong.blade.php
@@ -234,67 +234,8 @@
                 </div>
 
             </div>
-            <div class="w-full flex flex-col 2xl:w-1/3">
-                <div class="flex-1 bg-white rounded-lg shadow-xl p-8">
-                    <h4 class="text-xl text-gray-700 font-bold">Empresas Parceiras</h4>
-                    <div x-data="imageSlider"
-                        class="relative mx-auto max-w-full overflow-hidden rounded-md bg-gray-100 p-2 sm:p-4 grid min-h-fit w-full place-content-center">
-                        <div
-                            class="absolute right-5 top-5 z-10 rounded-full bg-gray-600 px-2 text-center text-sm text-white">
-                            <span x-text="currentIndex"></span>/<span x-text="images.length"></span>
-                        </div>
 
-                        <button @click="previous()"
-                            class="absolute left-5 top-1/2 z-10 flex h-11 w-11 -translate-y-1/2 items-center justify-center rounded-full bg-gray-100 shadow-md">
-                            <i class="fas fa-chevron-left text-2xl font-bold text-gray-500"></i>
-                        </button>
-
-                        <button @click="forward()"
-                            class="absolute right-5 top-1/2 z-10 flex h-11 w-11 -translate-y-1/2 items-center justify-center rounded-full bg-gray-100 shadow-md">
-                            <i class="fas fa-chevron-right text-2xl font-bold text-gray-500"></i>
-                        </button>
-
-                        <div class="relative h-80" style="width: 30rem">
-                            <template x-for="(image, index) in images">
-                                <div x-show="currentIndex == index + 1"
-                                    x-transition:enter="transition transform duration-300"
-                                    x-transition:enter-start="opacity-0" x-transition:enter-end="opacity-100"
-                                    x-transition:leave="transition transform duration-300"
-                                    x-transition:leave-start="opacity-100" x-transition:leave-end="opacity-0"
-                                    class="absolute top-0">
-                                    <img :src="image" alt="image" class="rounded-sm" />
-                                </div>
-                            </template>
-                        </div>
-                    </div>
-                    <script>
-                        document.addEventListener("alpine:init", () => {
-                            Alpine.data("imageSlider", () => ({
-                                currentIndex: 1,
-                                images: [
-                                    "https://unsplash.it/640/425?image=30",
-                                    "https://unsplash.it/640/425?image=40",
-                                    "https://unsplash.it/640/425?image=50",
-                                ],
-                                previous() {
-                                    if (this.currentIndex > 1) {
-                                        this.currentIndex = this.currentIndex - 1;
-                                    }
-                                },
-                                forward() {
-                                    if (this.currentIndex < this.images.length) {
-                                        this.currentIndex = this.currentIndex + 1;
-                                    }
-                                },
-                            }));
-                        });
-                    </script>
-
-
-
-                </div>
-
-            </div>
+            
         </div>
 
 </body>
