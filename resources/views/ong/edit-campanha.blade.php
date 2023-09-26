528c73fcc8f5a82c8df071ff6711640629f729bb
Olivia Scurciatto Fernandes <olivia.scurciatto@yahoo.com>
2023-09-26 15:25:10 -0300

    .

--- resources/views/ong/edit-campanha.blade.php
+++ resources/views/ong/edit-campanha.blade.php
@@ -65,6 +65,14 @@
                             </select>
                         </div>
                     </div>
+                    <div class="mt-7 sm:col-span-3">
+                        <label for="pix"
+                            class="block text-sm font-medium leading-6 text-gray-900">Adionar chave-pix</label>
+                        <div class="mt-2">
+                            <input type="text" name="pix" id="pix" 
+                                class="block w-7/12 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                        </div>
+                    </div>
                 </div>
 
                 <div class="ml-10 mr-10 border-b border-gray-900/10 pb-12">
